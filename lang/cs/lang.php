b45c10a6a2c8d46a49ba48e6851b3475b7b078eb
Michal Koutný <xm.koutny@gmail.com>
2015-08-20 23:19:20 +0200

    Added Czech translation

---
+++ lang/cs/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * English language file for cookielaw plugin
+ *
+ * @author Michal Koutny <michal@fykos.cz>
+ */
+
+// custom language strings for the plugin
+$lang['information'] = 'Tato stránka využívá cookies pro analýzu provozu. ' .
+        'Používáním stránky souhlasíte s ukládáním těchto cookies na vašem počítači.';
+$lang['consent'] = 'OK';
+$lang['details'] = 'Více informací';
+$lang['details_url'] = 'https://www.google.com/intl/cs/policies/technologies/cookies/';
+
+
+
+//Setup VIM: ex: et ts=4 :
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -10,7 +10,7 @@ $lang['information'] = 'This website uses cookies for visitor traffic analysis. 
         'By using the website, you agree with storing the cookies on your computer.';
 $lang['consent'] = 'OK';
 $lang['details'] = 'More information';
-$lang['details_url'] = 'https://www.google.de/intl/en/policies/technologies/cookies/';
+$lang['details_url'] = 'https://www.google.com/intl/en/policies/technologies/cookies/';
 
 
 

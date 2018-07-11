5ebb7bb4698ea34d9be1297c73e06645741634f1
nikopp <nikop@web.de>
2018-07-11 21:39:38 +0200

    Create lang.php for de-informal

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * German language file (de-informal) for cookielaw plugin
+ *
+ * @author Niko P. <nikop@web.de>
+ */
+
+$lang['information'] = 'Cookies helfen bei der Bereitstellung von Inhalten. ' .
+        'Durch die Nutzung dieser Seiten erklärst du dich damit einverstanden, dass Cookies auf deinem Rechner gespeichert werden. ';
+$lang['consent'] = 'OK';
+$lang['details'] = ' Weitere Information';
+$lang['details_url'] = 'https://de.wikipedia.org/wiki/Cookie';

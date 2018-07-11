77fff1a980ee83092a5be7d7682ce1c709ec2d9f
nikopp <nikop@web.de>
2018-07-11 21:45:43 +0200

    Create lang.php for Spanish translation

---
+++ es/lang.php
@@ -0,0 +1,11 @@
+    <?php
+    /**
+     * Spanish language file for cookielaw plugin
+     *
+     * from https://www.dokuwiki.org/plugin:cookielaw#configuration_and_settings
+     */ 
+    $lang['information'] = 'Esta página web emplea cookies y registra la dirección IP para analizar su tráfico y por motivos de seguridad. ' .
+                           'Al permanecer en este sitio, acepta que se almacenen cookies en su ordenador. ';
+    $lang['consent']     = 'De acuerdo';
+    $lang['details']     = 'Más información';
+    $lang['details_url'] = 'https://es.wikipedia.org/wiki/Cookie_(inform%C3%A1tica)'

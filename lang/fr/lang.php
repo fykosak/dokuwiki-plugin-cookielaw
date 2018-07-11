7107e1e54eab76b0caa355070cde7c7daf6b72ad
nikopp <nikop@web.de>
2018-07-11 21:44:09 +0200

    Create lang.php for french translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+    <?php
+    /**
+     * French language file for cookielaw plugin
+     *
+     * from https://www.dokuwiki.org/plugin:cookielaw#configuration_and_settings
+     */ 
+    $lang['information'] = 'Ce site web utilise des cookies pour analyser le trafic de visites. ' .
+                           'En restant sur ce site, vous acceptez le stockage de cookies sur votre ordinateur. ';
+    $lang['consent']     = 'OK';
+    $lang['details']     = 'En savoir plus';
+    $lang['details_url'] = 'https://fr.wikipedia.org/wiki/Cookie_(informatique)';

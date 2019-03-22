4bdb87731a2afdb4c707adc939624d8880695c9d
Christian W <doctore74@users.noreply.github.com>
2019-03-22 01:03:20 +0100

    Update lang.php
    Hopefully a usable change for EU Cookie Law.

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -2,16 +2,19 @@
 /**
  * German language file for cookielaw plugin
  *
- * @author Django <django@nausch.org>
+ * @author Django <django@nausch.org> / Doc <doc@snowheaven.de>
  */
 
 // custom language strings for the plugin
-$lang['information'] = 'Cookies helfen bei der Bereitstellung von Inhalten. ' .
-        'Durch die Nutzung dieser Seiten erklären Sie sich damit einverstanden, dass Cookies auf Ihrem Rechner gespeichert werden. ';
-$lang['consent'] = 'OK';
-$lang['details'] = ' Weitere Information';
-$lang['details_url'] = 'https://de.wikipedia.org/wiki/Cookie';
-
-
+$lang['information']  = 'Cookies helfen bei der Bereitstellung von Inhalten. ' .
+                        'Diese Website verwendet Cookies. Mit der Nutzung der Website erklären Sie sich damit einverstanden, '.
+                        'dass Cookies auf Ihrem Computer gespeichert werden. Außerdem bestätigen Sie, '.
+                        'dass Sie unsere Datenschutzerklärung gelesen und verstanden haben. Wenn Sie nicht einverstanden sind, '.
+                        'verlassen Sie die Website.';
+$lang['consent']      = 'OK';
+$lang['details']      = ' Weitere Information';
+$lang['details_url']  = 'https://de.wikipedia.org/wiki/Cookie';
+$lang['details2']     = 'Weitere Informationen ueber die Verwendung von Cookies in Dokuwiki';
+$lang['details_url2'] = 'https://www.dokuwiki.org/faq:cookies';
 
 //Setup VIM: ex: et ts=4 :

c02d799df8fc9b5523350b41b2327992e6888d06
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-05-31 08:57:33 +1000

    Create settings.php

---
+++ lang/uk/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Ukrainian language file for the configuration manager
+ *
+ * @author     Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['position']          = 'Розташування інформаційного банера про файлах cookie на сторінці';
+$lang['position_o_top']    = 'вгорі';
+$lang['position_o_bottom'] = 'внизу';

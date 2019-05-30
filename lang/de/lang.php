f5223ffeff95aafb759a75eb0db65a39cbc9268f
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-05-31 08:51:39 +1000

    Update lang.php

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,16 +5,7 @@
  * @author Django <django@nausch.org> / Doc <doc@snowheaven.de>
  */
 
-// custom language strings for the plugin
-$lang['information']  = 'Cookies helfen bei der Bereitstellung von Inhalten. ' .
-                        'Diese Website verwendet Cookies. Mit der Nutzung der Website erklären Sie sich damit einverstanden, '.
-                        'dass Cookies auf Ihrem Computer gespeichert werden. Außerdem bestätigen Sie, '.
-                        'dass Sie unsere Datenschutzerklärung gelesen und verstanden haben. Wenn Sie nicht einverstanden sind, '.
-                        'verlassen Sie die Website.';
+$lang['information']  = 'Diese Website verwendet Cookies. Durch die Nutzung der Website stimmen Sie dem Speichern von Cookies auf Ihrem Computer zu. Außerdem bestätigen Sie, dass Sie unsere Datenschutzbestimmungen gelesen und verstanden haben. Wenn Sie nicht einverstanden sind, verlassen Sie die Website.';
 $lang['consent']      = 'OK';
 $lang['details']      = ' Weitere Information';
 $lang['details_url']  = 'https://de.wikipedia.org/wiki/Cookie';
-$lang['details2']     = 'Weitere Informationen ueber die Verwendung von Cookies in Dokuwiki';
-$lang['details_url2'] = 'https://www.dokuwiki.org/faq:cookies';
-
-//Setup VIM: ex: et ts=4 :

8970ff284a67613d8561995a6e2bd7575569c5a4
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-05-31 08:50:47 +1000

    Update lang.php

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -5,16 +5,7 @@
  * @author Niko P. <nikop@web.de> / Doc <doc@snowheaven.de>
  */
 
-// Dokuwiki
-$lang['information']  = 'Cookies helfen bei der Bereitstellung von Inhalten. ' .
-                        'Diese Website verwendet Cookies. Mit der Nutzung der Website erklären Sie sich damit einverstanden, '.
-                        'dass Cookies auf Ihrem Computer gespeichert werden. Außerdem bestätigen Sie, '.
-                        'dass Sie unsere Datenschutzerklärung gelesen und verstanden haben. Wenn Sie nicht einverstanden sind, '.
-                        'verlassen Sie die Website.';
+$lang['information']  = 'Cookies helfen bei der Bereitstellung von Inhalten. Diese Website verwendet Cookies. Mit der Nutzung der Website erklären Sie sich damit einverstanden, dass Cookies auf Ihrem Computer gespeichert werden. Außerdem bestätigen Sie, dass Sie unsere Datenschutzerklärung gelesen und verstanden haben. Wenn Sie nicht einverstanden sind, verlassen Sie die Website.';
 $lang['consent']      = 'OK';
 $lang['details']      = ' Weitere Information';
 $lang['details_url']  = 'https://de.wikipedia.org/wiki/Cookie';
-$lang['details2']     = 'Weitere Informationen ueber die Verwendung von Cookies in Dokuwiki';
-$lang['details_url2'] = 'https://www.dokuwiki.org/faq:cookies';
-
-//Setup VIM: ex: et ts=4 :

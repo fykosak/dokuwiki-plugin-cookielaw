ceec9943e42aaa03058431b7805db9e2b3d1bfd9
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-05-31 08:53:43 +1000

    Update lang.php

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,11 +1,11 @@
-    <?php
-    /**
-     * French language file for cookielaw plugin
-     *
-     * from https://www.dokuwiki.org/plugin:cookielaw#configuration_and_settings
-     */ 
-    $lang['information'] = 'Ce site web utilise des cookies pour analyser le trafic de visites. ' .
-                           'En restant sur ce site, vous acceptez le stockage de cookies sur votre ordinateur. ';
-    $lang['consent']     = 'OK';
-    $lang['details']     = 'En savoir plus';
-    $lang['details_url'] = 'https://fr.wikipedia.org/wiki/Cookie_(informatique)';
+<?php
+/**
+ * French language file for cookielaw plugin
+ *
+ * @author IP 176.135.33.238
+ */ 
+
+$lang['information'] = 'Ce site web utilise des cookies. En utilisant le site Web, vous acceptez le stockage de cookies sur votre ordinateur. Vous reconnaissez également que vous avez lu et compris notre politique de confidentialité. Si vous n\'êtes pas d\'accord, quittez le site.';
+$lang['consent']     = 'OK';
+$lang['details']     = 'En savoir plus';
+$lang['details_url'] = 'https://fr.wikipedia.org/wiki/Cookie_(informatique)';

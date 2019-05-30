0a2b4dfa92921105676f9847db5f408aef13fe2c
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-05-31 08:55:34 +1000

    Create lang.php

---
+++ lang/pl/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Polish language file for cookielaw plugin
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['information'] = 'Ta strona używa plików cookie. Będąc tutaj, zgadzasz się na ich przechowywanie na swoim komputerze. Użytkownik potwierdza również, że przeczytał i zrozumiał naszą Politykę prywatności. Jeśli się nie zgadzasz - opuść stronę.';
+$lang['consent'] = 'OK';
+$lang['details'] = 'Więcej informacji na temat plików cookie';
+$lang['details_url'] = 'https://pl.wikipedia.org/wiki/HTTP_cookie';

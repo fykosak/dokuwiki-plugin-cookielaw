34a88622a94d08e67a66748ca35e41b7c805eb9e
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-06-01 00:31:36 +1000

    Update settings.php

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -5,6 +5,6 @@
  * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
 
-$lang['position']      = 'La ubicación del banner de información sobre cookies en la página';
+$lang['position']          = 'La ubicación del banner de información sobre cookies en la página';
 $lang['position_o_top']    = 'superior';
 $lang['position_o_bottom'] = 'inferior';

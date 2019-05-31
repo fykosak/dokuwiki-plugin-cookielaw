30e39bfeeb0f6d664fed8b47069c84ae7a1408a9
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-06-01 00:53:32 +1000

    Update lang.php

--- lang/fi/lang.php
+++ lang/fi/lang.php
@@ -2,7 +2,7 @@
 /**
  * Finnish language file for cookielaw plugin
  *
- * @author Michal Koutny <michal@fykos.cz> / Doc <doc@snowheaven.de>
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
 
 $lang['information'] = 'Tämä sivusto käyttää evästeitä. Käyttämällä sivustoa hyväksyt evästeiden tallentamisen tietokoneellesi. Lisäksi hyväksyt, että olet lukenut ja ymmärtänyt tietosuojakäytäntömme. Jos et ole samaa mieltä, jätä sivusto.';

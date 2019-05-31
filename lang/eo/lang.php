7e983d49b2c091dc6a99a41f6da69a3e41a74d84
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-06-01 00:53:31 +1000

    Update lang.php

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -2,7 +2,7 @@
 /**
  * Esperanto language file for cookielaw plugin
  *
- * @author Michal Koutny <michal@fykos.cz> / Doc <doc@snowheaven.de>
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
 
 $lang['information'] = 'Ĉi tiu retejo uzas kuketojn. Uzante la retejon, vi konsentas kun konservado de kuketoj sur via komputilo. Vi ankaŭ agnoskas, ke vi legis kaj komprenas nian Privatecan Politikon. Se vi ne konsentas, lasu la retejon.';

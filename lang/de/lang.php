49436e4f113b69bbb1e2a03778bc3fd30980e1bd
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-06-01 00:00:52 +1000

    Update lang.php

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -7,5 +7,5 @@
 
 $lang['information']  = 'Diese Website verwendet Cookies. Durch die Nutzung der Website stimmen Sie dem Speichern von Cookies auf Ihrem Computer zu. Außerdem bestätigen Sie, dass Sie unsere Datenschutzbestimmungen gelesen und verstanden haben. Wenn Sie nicht einverstanden sind, verlassen Sie die Website.';
 $lang['consent']      = 'OK';
-$lang['details']      = ' Weitere Information';
+$lang['details']      = 'Weitere Information';
 $lang['details_url']  = 'https://de.wikipedia.org/wiki/Cookie';

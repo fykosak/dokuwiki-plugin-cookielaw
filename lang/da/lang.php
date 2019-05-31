ddae4f4a3d3868dd5d09717ba9da93c74f37aece
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-06-01 00:46:59 +1000

    Create lang.php

---
+++ lang/da/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Danish language file for cookielaw plugin
+ *
+ * @author Django <django@nausch.org> / Doc <doc@snowheaven.de>
+ */
+
+$lang['information']  = 'Denne hjemmeside bruger cookies. Ved at bruge hjemmesiden accepterer du at gemme cookies på din computer. Du anerkender også, at du har læst og forstået vores privatlivspolitik. Hvis du ikke er enig, forlad hjemmesiden.';
+$lang['consent']      = 'OK';
+$lang['details']      = 'Flere oplysninger om cookies';
+$lang['details_url']  = 'https://da.wikipedia.org/wiki/Cookie';

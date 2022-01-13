dbd00f84e519d5198664c565bc50474ae309768c
Viktor Kristian <vkristian@gmail.com>
2022-01-13 12:47:00 +0100

    added SK lang translation

---
+++ lang/sk/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Slovak language file for cookielaw plugin
+ *
+ * @author Viktor Kristian <vkristian@gmail.com>
+ */
+$lang['information'] = 'Tento web používa cookies. Používáním týchto stránok súhlasíte s ukladáním cookies do vášho počítača. Tak isto beriete na vedomie, že ste si prečítali a porozumeli našim Zásadám ochrany osobných údajov. Ak nesúhlasíte, prosím opustite tieto stránky.';
+$lang['consent']     = 'OK';
+$lang['details']     = 'Viac informácií';
+$lang['details_url'] = 'https://sk.wikipedia.org/wiki/HTTP_cookie';
---
+++ lang/sk/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Slovak language file for the configuration manager
+ *
+ * @author Viktor Kristian <vkristian@gmail.com>
+ */
+$lang['position']          = 'Umiestnenie informačného banneru o cookies na stránke';
+$lang['position_o_top']    = 'hore';
+$lang['position_o_bottom'] = 'dole';
